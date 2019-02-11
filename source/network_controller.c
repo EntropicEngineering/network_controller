94e236170f4ddc657616b39feea98da0d3398f84
Peter O'Hanley <gus@entropicengineering.com>
2019-02-11 13:29:39 -0600

    update main test

--- source/network_controller.c
+++ source/network_controller.c
@@ -188,6 +188,16 @@ normal_read_command_step4(uint8_t bcm_addr)
   }
 }
 
+/* Write the bytes in the buffer result to the bcm location oset in page.
+ */
+int
+normal_write_operation(uint8_t page, uint8_t oset
+                      , uint8_t *result, size_t len)
+{
+  //TODO implement normal_write_operation
+  assert(0);
+}
+
 typedef struct {uint8_t page; uint8_t reg; uint8_t len;} bcm_reg;
 #define BCM_53128_PAGE_STATUS_REGISTERS 0x01
 #define BCM_53128_STATUS_LAST_SOURCE_ADDRESS_PORT_0 ((bcm_reg) {0x01, 0x10, 6})
@@ -202,6 +212,31 @@ typedef struct {uint8_t page; uint8_t reg; uint8_t len;} bcm_reg;
 #define AS_LEN(a,b,c) c
 
 #define BCM_LAST_MAC_PORT_3 ((bcm_reg) {0x01, 0x22, 6})
+
+struct macaddr {uint8_t addr[6];};
+int last_mac(unsigned int port, struct macaddr *m);
+int last_mac(unsigned int port, struct macaddr *m)
+{
+  if (port > 8u) {
+    memset(m, 0, sizeof(struct macaddr));
+    return -2;
+  }
+  return normal_read_operation(0x01, 0x10 + 6*port, m->addr, 6);
+}
+
+int
+set_port_enables(int port, bool txen, bool rxen)
+{
+  if (port > 7) {
+    return -2;
+  }
+  uint8_t port_ctl_status;
+  int readret = normal_read_operation(0x00, port, &port_ctl_status, 1);
+  //TODO abort if read failed
+  port_ctl_status &= 0xfc | ((!!txen) << 1) | (!!rxen);
+  return normal_write_operation(0x00, port, &port_ctl_status, 1);
+}
+
 int main(void)
 {
   BOARD_BootClockRUN();
@@ -221,8 +256,13 @@ int main(void)
     //Serial.printf("getting link status summary\n");
     //get mac address (see datasheet for which)
     //uint8_t macbuf[BCM_53128_STATUS_LAST_SOURCE_ADDRESS_PORT_2.len] = {0};
-    uint8_t macbuf[6] = {0};
-    normal_read_operation(0x01, 0x1c, macbuf, 6);
+
+    struct macaddr macs[8];
+    int rets[8] = {0};
+
+    for (int i = 0; i < 8; i++) {
+      rets[i] = last_mac(i, &macs[i]);
+    }
 
     //TODO pause to inspect values in gdb
   }
